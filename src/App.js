eda3f120aa2a706dbd95401f9cfd8a8d38716ecb
Poornendra Parouha <poornendraparouha@gmail.com>
2024-08-04 21:27:05 +0530

    removed login pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-// import Blog from "./Components/Blog";
-import { useState } from "react";
-import Login from "./Components/loginPage";
-import Reset from "./Components/resetPassword";
+import Blog from "./Components/Blog";
 
 function App() {
-const [form, setForm] = useState("Login");
-
   return (
-    <div className="App">
-      <h1>Welcome!!</h1>
-    {form === "Login"? <Login/> : <Reset/> }
-    <br/>
-    <button onClick={()=>{setForm(form === "Login"? "Reset" : "Login")}}>
-      {form === "Login"? "Forgot Password" : "Back to Login" }
-    </button>
-
-    </div>
-    // <>
-    //   <Blog />
-    // </>
+    <>
+      <Blog />
+    </>
   );
 }
 
